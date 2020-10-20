a7094106b622925e5e1ee9266060598d6e5dba0e
Satantra Tewari <satantra1508@gmail.com>
2020-10-20 22:44:56 +0530

    [satantra] welcome message displayed

---
+++ Maximum_Value_Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maximum_Value_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum Value Problem using Generics");
+        }
+    }
+}
